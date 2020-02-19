e10ef56a7d314dddac9787c0213eea082fbe69f9
TebohoRomeo <teboholekhalo13@gmail.com>
2020-02-19 19:29:32 +0200

    Still working on slot

--- src/calculator.js
+++ src/calculator.js
@@ -1,41 +1,47 @@
-let setSlot = [], 
-LAST = 0;
+let setSlot = [],
+  LAST = 0;
 
 class Calculator {
-  constructor() {
-  }
-  // I am done with the ad function, dont ever touch it again...//
+  constructor() {}
   add() {
     let sum = 0;
     let i = 0;
-    for (i ; i < arguments.length; i++) {
+    for (i; i < arguments.length; i++) {
       sum += arguments[i];
     }
-    
-    setSlot.push(sum)
+
+    setSlot.push(sum);
     return sum;
   }
   multiply() {
     let product = 1;
     let i = 0;
-    for (i ; i < arguments.length; i++) {
+    for (i; i < arguments.length; i++) {
       product *= arguments[i];
     }
-    
-    setSlot.push(product)
+
+    setSlot.push(product);
     return product;
   }
   last() {
-    LAST = setSlot[setSlot.length - 1]
+    LAST = setSlot[setSlot.length - 1];
     return LAST;
+  }
+
+  set_slot(num) {
+    this.lastNum = this.slot[num - 1];
+  }
+
+  get_slot(num) {
+    return this.slot[num - 1];
   }
 }
 
-let casio = new Calculator()
-console.log(casio.add(1, 2))
-console.log(casio.last(), 'Last result which is 3');
-console.log(casio.multiply(1, 2))
-console.log(casio.last(), 'Last result which is ');
+let casio = new Calculator();
+console.log(casio.add(1, 2));
+console.log(casio.last(), 'Should return 3'); // Addition works.
+console.log(casio.multiply(1, 2));
+console.log(casio.last(), 'Should return 2'); // Multiply works.
+
 
 module.exports = Calculator;
-
