a2c3f01bad4cd50d7d65e23690ffd36ba7eac3be
TebohoRomeo <teboholekhalo13@gmail.com>
2020-02-19 21:41:59 +0200

    Work on the last and slot_

--- src/calculator.js
+++ src/calculator.js
@@ -1,31 +1,49 @@
-let setSlot = [],
-  LAST = 0;
-
 class Calculator {
-  constructor() {}
-  add() {
+  constructor() {
+    this.lastNum = 0;
+    this.slot = [];
+  }
+
+  //add numbers
+  add(...args) {
     let sum = 0;
-    let i = 0;
-    for (i; i < arguments.length; i++) {
-      sum += arguments[i];
+
+    for (let num of args) {
+      if (num == 'Last') {
+        return this.last();
+      }
+
+      if (num == 'slot_') {
+        let slot_num = num[num.length - 1];
+        return this.slot[slot_num - 1];
+      }
+      sum += num;
     }
 
-    setSlot.push(sum);
+    this.lastNum = sum;
+    this.slot.push(sum);
+
     return sum;
   }
-  multiply() {
-    let product = 1;
-    let i = 0;
-    for (i; i < arguments.length; i++) {
-      product *= arguments[i];
-    }
 
-    setSlot.push(product);
+  multiply(...args) {
+    let product = 1;
+
+    for (let num of args) {
+      if (num == 'Last') {
+        num = this.last();
+      }
+      product *= num;
+    }
+    this.lastNum = product;
+    this.slot.push(product);
     return product;
   }
+
+ // Don't modify any code below this one. You are done with this part
+
   last() {
-    LAST = setSlot[setSlot.length - 1];
-    return LAST;
+    return this.lastNum;
   }
 
   set_slot(num) {
@@ -38,10 +56,13 @@ class Calculator {
 }
 
 let casio = new Calculator();
-console.log(casio.add(1, 2));
-console.log(casio.last(), 'Should return 3'); // Addition works.
-console.log(casio.multiply(1, 2));
-console.log(casio.last(), 'Should return 2'); // Multiply works.
+console.log(casio.add(100, 200), 'should return 300')
+console.log(casio.add(10, 20), 'should return 30');
+console.log(casio.get_slot(1), 'should return 300');
+console.log(casio.get_slot(2), 'should return 30');
+console.log(casio.last(), 'should return 30');
+console.log(casio.add('Last', 10), 'should return 40 for using last'); // Last part to work on. Go to Mudi for help.
+console.log(casio.add('slot_1', 10), 'should return 30 pics'); // Last part to work on. Go to Mudi for help.
 
 
 module.exports = Calculator;
