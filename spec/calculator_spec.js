cb637fe9b6a007a21d63778d102c491d13e42648
TebohoRomeo <teboholekhalo13@gmail.com>
2020-02-20 14:25:59 +0200

    Done an Dusted

--- spec/calculator_spec.js
+++ spec/calculator_spec.js
@@ -1,10 +1,55 @@
-let Calculator = require('../src/calculator')
+describe('calculate the total', function() {
+  beforeEach(() => {
+    global.Calculator = require('../src/calculator');
+    global.calculator_instance = new Calculator();
+  });
 
-let calculator_instance = new Calculator();
+  it('Checks if add() is defined', function() {
+    expect(calculator_instance.add()).toBeDefined();
+  });
 
- describe('Last() function', () => {
-    it('should take last as an argument', function(){
-        expect(calculator_instance.add(1,2)).toEqual(3);
-        expect(calculator_instance.last()).toEqual(3)
-    })
- })
+  it('Checks if multiply() is defined', function() {
+    expect(calculator_instance.multiply()).toBeDefined();
+  });
+
+  it('should sum the two numbers', function() {
+    expect(calculator_instance.add(0, 0)).toBe(0);
+  });
+
+  it('should display the sum', function() {
+    expect(calculator_instance.add(1, 2, 3, 4)).toBe(10);
+  });
+
+  it('should return 10', function() {
+    expect(calculator_instance.add(2, 3, 5)).toBe(10);
+  });
+
+  it('should return 8', function() {
+    expect(calculator_instance.add(3, 5)).toBe(8);
+  });
+
+  it('should give product of two numbers', function() {
+    expect(calculator_instance.multiply(1, 2)).toBe(2);
+  });
+
+  it('should return 60 ', function() {
+    expect(calculator_instance.multiply(30, 2)).toBe(60);
+  });
+
+  it('should give the product of the array', function() {
+    expect(calculator_instance.multiply(1, 2, 3, 4)).toBe(24);
+  });
+
+  //memory slots
+
+  it('Should take single number as slot number', function() {
+    calculator_instance.add(5, 4);
+    expect(calculator_instance.get_slot(1)).toEqual(9);
+  });
+
+  it('Should take slot number and add with a number', function() {
+    calculator_instance.add(5, 4);
+    calculator_instance.get_slot(1);
+    expect(calculator_instance.add('slot_1', 2)).toBe(11);
+  });
+});
--- src/calculator.js
+++ src/calculator.js
@@ -1,23 +1,23 @@
+//Author : Romeo and Kurtlin
+'use strict';
+
 class Calculator {
   constructor() {
     this.lastNum = 0;
     this.slot = [];
   }
-
-  //add numbers
+  
   add(...args) {
     let sum = 0;
-
+    
     for (let num of args) {
       if (num == 'Last') {
-        return this.last();
+        num = this.last();
+      } else if (num == 'slot_' + num.toString().charAt(num.length - 1)) {
+        sum += this.get_slot(num.charAt(num.length - 1));
+      } else if (!isNaN(num)) {
+        sum += num;
       }
-
-      if (num == 'slot_') {
-        let slot_num = num[num.length - 1];
-        return this.slot[slot_num - 1];
-      }
-      sum += num;
     }
 
     this.lastNum = sum;
@@ -32,15 +32,19 @@ class Calculator {
     for (let num of args) {
       if (num == 'Last') {
         num = this.last();
+      } else if (num == 'slot_' + num.toString().charAt(num.length - 1)) {
+        product *= this.get_slot(num.charAt(num.length - 1));
+      } else if (!isNaN(num)) {
+        product *= num;
       }
-      product *= num;
     }
     this.lastNum = product;
     this.slot.push(product);
+
     return product;
   }
 
- // Don't modify any code below this one. You are done with this part
+  // Don't modify any code below this one. You are done with this part
 
   last() {
     return this.lastNum;
@@ -54,15 +58,5 @@ class Calculator {
     return this.slot[num - 1];
   }
 }
-
-let casio = new Calculator();
-console.log(casio.add(100, 200), 'should return 300')
-console.log(casio.add(10, 20), 'should return 30');
-console.log(casio.get_slot(1), 'should return 300');
-console.log(casio.get_slot(2), 'should return 30');
-console.log(casio.last(), 'should return 30');
-console.log(casio.add('Last', 10), 'should return 40 for using last'); // Last part to work on. Go to Mudi for help.
-console.log(casio.add('slot_1', 10), 'should return 30 pics'); // Last part to work on. Go to Mudi for help.
-
 
 module.exports = Calculator;
